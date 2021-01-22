3dc2140dc3717a11287950859713cf17f3cd7871
Mohammad Al-Haddad <Moh.jalhaddad@gmail.com>
2021-01-22 15:04:23 +0300

    Nothing New

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-
-def create_app(test_config=None):
-    app = Flask(__name__)
-
-    return app
+app = Flask(__name__)
 
 
-APP = create_app()
+@app.route('/')
+def index():
+    return "Hello World"
+
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
