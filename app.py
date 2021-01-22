aa35346dcf2d8a2c3a2aadfe812ffa0351b95e37
Mohammad Al-Haddad <Moh.jalhaddad@gmail.com>
2021-01-22 17:43:15 +0300

    Added DB,Updated Requierments

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from auth import AuthError, requires_auth
+from models import setup_db, Movie, Actor
 
 app = Flask(__name__)
+setup_db(app)
+CORS(app)
 
 
 @app.route('/')
--- models.py
+++ models.py
@@ -1,1 +1,152 @@
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
 
+database_path = "postgres://{}/{}".format('localhost:5432', 'capstone')
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+'''
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+
+class Movie(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(DateTime)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    '''
+    insert()
+        inserts a new model into a database
+        EXAMPLE
+            movie = Movie(title, release_date)
+            movie.insert()
+    '''
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie = Movie(title, release_date)
+            movie.delete()
+    '''
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie = Movie.query.filter(Movie.id == id).one_or_none()
+            movie.title = 'Black Coffee'
+            movie.update()
+    '''
+
+    def update(self):
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+    def __repr__(self):
+        return json.dumps(self.format())
+
+
+class Actor(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    '''
+    insert()
+        inserts a new model into a database
+        EXAMPLE
+            actor = Actor(name, age,gender)
+            actor.insert()
+    '''
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            actor = Actor(name, age,gender)
+            actor.delete()
+    '''
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            actor = Actor.query.filter(Actor.id == id).one_or_none()
+            actor.name = 'Black Coffee'
+            actor.update()
+    '''
+
+    def update(self):
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+    def __repr__(self):
+        return json.dumps(self.format())
