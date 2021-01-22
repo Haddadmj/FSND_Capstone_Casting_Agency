5959db087c808cb372547e4236e40b94a11ace65
Mohammad Al-Haddad <Moh.jalhaddad@gmail.com>
2021-01-22 14:35:08 +0300

    Nothing New

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 
-@app.route('/')
-def index():
-    return 'Hello World'
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
 
+    @app.route('/')
+    def index():
+        return 'Hello World'
+
+    return app
+
+
+APP = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run(host='0.0.0.0', port=8080, debug=True)
--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+export AUTH0_DOMAIN = ''
+export ALGORITHMS = ['RS256']
+export API_AUDIENCE = ''
+export CLIENT_ID = ''
 
+# theses are the needed tokens for three deferent roles:
+export Executive_jwt = ''
+export Director_jwt = ''
+export Assistant_jwt = ''
