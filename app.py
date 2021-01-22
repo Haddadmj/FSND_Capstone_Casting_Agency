6c209107b442e980689ba281dcfd69fce8848945
Mohammad Al-Haddad <Moh.jalhaddad@gmail.com>
2021-01-22 14:24:47 +0300

    Changed Host IP and port number

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
-import os
-from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__)
-    CORS(app)
+@app.route('/')
+def index():
+    return 'Hello World'
 
-    @app.route('/')
-    def index():
-        return "Hello World"
-
-    return app
-
-
-APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
